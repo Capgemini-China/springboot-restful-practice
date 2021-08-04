913b92c86a2fe07c1fb5ff04970fe180e7ae8145
Lingrui <392425595@qq.com>
2021-08-04 22:47:01 +0800

    获取employee列表

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -1,5 +1,26 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Employee;
+import com.example.employee.restfulapi.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/employees")
 public class EmployeeController {
-    //在此处完成Employee API
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping(value = "")
+    public ResponseEntity<List<Employee>> getAllEmployees() throws Exception {
+        List<Employee> employeeList = employeeRepository.findAll();
+        return new ResponseEntity<>(employeeList, HttpStatus.OK);
+    }
 }
