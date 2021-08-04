c49431168e563fca39e293e7a18dff8c726cfdce
Lingrui <392425595@qq.com>
2021-08-04 22:47:43 +0800

    获取某个具体employee

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class EmployeeController {
     public ResponseEntity<List<Employee>> getAllEmployees() throws Exception {
         List<Employee> employeeList = employeeRepository.findAll();
         return new ResponseEntity<>(employeeList, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Employee> getCompanyById(@PathVariable Long id) throws Exception {
+        Employee employee = employeeRepository.getById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 }
